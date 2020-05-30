bcc1178d24b8c514814c111032086b7f988e822c
CarlaHeras <61461630+CarlaHeras@users.noreply.github.com>
2020-05-31 00:38:53 +0200

    Create DadesAPI.java

---
+++ src/project/DadesAPI.java
@@ -0,0 +1,11 @@
+  
+package project;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DadesAPI {
+//Creem 3 llistes on guardarem les dades. Ho fem amb llistes per a que ens sigui més senzill afegir elements.
+    static List<String> estudiants = new ArrayList<>();
+    static List<String> personatges = new ArrayList<>();
+    static List<String> professors = new ArrayList<>();
+}
