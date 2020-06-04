77f58b83ff65ac9d879ac32714f83d0241c11299
carla <c.heras@sapalomera.cat>
2020-06-04 16:15:09 +0200

    Opció esborrar i detector array buit

--- src/project/GestorDades.java
+++ src/project/GestorDades.java
@@ -9,6 +9,7 @@ import project.model.DadesAPI;
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
 
 public class GestorDades {
     private DadesAPI dades;
@@ -16,23 +17,22 @@ public class GestorDades {
         dades = new DadesAPI();
     }
 
-    /*//Declarem les 3 arrays a on passarem les dades de les llistes de la classe DadesAPI quan les necessitem en forma d'array.
-    public static String[] estudiants;
-    public static String[] professors;
-    public static String[] personatges;
-    */
     /**
      * Carrega des de l'API les dades que ens interessin.
      *
      */
-    //Invoquem el mètode que ens carrega les dades de l'API a les llistes de la classe DadesAPI i a continuació les conver-
-    //-tim a array, a les 3 que hem creat adalt.
+    //Invoquem el mètode que ens carrega les dades de l'API
     public void carregarDades() {
         carregarEstudiants();
         carregarProfessors();
 
     }
 
+    public DadesAPI getDades() {
+        return dades;
+    }
+
+    //CARREGAR ELS ATRIBUTS DELS ESTUDIANTS
     public void carregarEstudiants() {
         try {
             URL url = new URL("http://hp-api.herokuapp.com/api/characters/students");
@@ -45,15 +45,19 @@ public class GestorDades {
                 String nom = person.get("name").toString();
                 String especie = person.get("species").toString();
                 String casa = person.get("house").toString();
+                String patronus = person.get("patronus").toString();
                 String actor = person.get("actor").toString();
-                Estudiant est = new Estudiant(nom, especie, casa, actor);
+                Estudiant est = new Estudiant(nom, especie, casa, patronus, actor);
                 dades.afegirEstudiant(est);
             }
             connection.disconnect();
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
+
     }
+
+    //CARREGAR ELS ATRIBUTS DELS PROFESSORS
     public void carregarProfessors() {
         try {
             URL url = new URL("http://hp-api.herokuapp.com/api/characters/staff");
@@ -66,13 +70,39 @@ public class GestorDades {
                 String nom = person.get("name").toString();
                 String especie = person.get("species").toString();
                 String casa = person.get("house").toString();
+                String patronus = person.get("patronus").toString();
                 String actor = person.get("actor").toString();
-                Professor prof = new Professor(nom, especie, casa, actor);
+                Professor prof = new Professor(nom, especie, casa, patronus, actor);
                 dades.afegirProfessor(prof);
             }
             connection.disconnect();
         } catch (Exception e) {
             System.out.println(e.getMessage());
+        }
+    }
+
+    //RECORRE ELS ARRAYLISTS BUSCANT EL NOM QUE S'HA INSERIT I L'ELIMINA
+    public void buscareliminarPersonatge(String nom) {
+        for (int i = 0; i < dades.getEstudiants().size() ; i++) {
+            if (nom.equals(dades.getEstudiants().get(i).getNom().toLowerCase())) {
+                dades.getEstudiants().remove(i);
+                System.out.println("S'ha eliminat " + nom);
+            }
+        }
+        for (int i = 0; i < dades.getProfessors().size(); i++) {
+            if (nom.equals(dades.getProfessors().get(i).getNom().toLowerCase())) {
+                dades.getProfessors().remove(i);
+                System.out.println("S'ha eliminat " + nom);
+            }
+        }
+    }
+
+    //DETECTA SI L'ARRAY ESTÀ BUIDA I RETORNA UN BOLEÀ
+    public boolean comprovarArraybuida(ArrayList a) {
+        if (a.size() == 0) {
+            return true;
+        } else {
+            return false;
         }
     }
 }
