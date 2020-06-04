679082e31e235c89a9ecb9eea2321132eb2cd13c
carla <c.heras@sapalomera.cat>
2020-06-04 17:41:38 +0200

    Menu afegir personatges amb submenu per alumnes i professors

--- src/project/M3_UF3_ActAvaluable.java
+++ src/project/M3_UF3_ActAvaluable.java
@@ -1,9 +1,7 @@
 package project;
 
-        import project.model.DadesAPI;
-
         import java.io.IOException;
-        import java.util.Scanner;
+import java.util.Scanner;
 
 public class M3_UF3_ActAvaluable {
 
@@ -20,68 +18,50 @@ public class M3_UF3_ActAvaluable {
     public void inici() throws IOException {
 
         Scanner sc = new Scanner(System.in);
-        DadesAPI dades = new DadesAPI();
-        String usuari;
-        Interficie.mostrarMissatge("Inserta el teu nom:");
-        usuari = sc.nextLine();
-        GestorDades.guardarUsuari(usuari);
-        int eleccio = -1;       //Variable per guardar l'opció del menú secundari elegida per l'usuari.
-        int eleccio2 = -1;      //Variable per guardar l'opció del menú secundari elegida per l'usuari.
-        int id_consulta = 0;    //Variable per identificar les consultes en el moment d'afegir elements.
-        int haconsultat = 0;    //Variable per guardar si s'ha fet consulta.
-        String afegirelement;   //Variable per preguntar si es vol afegir element, serà Y o N
-        String nomelement;      //Variable on guardarem l'element que es vulgui afegir.
-        String volsordenar;     //Variable per preguntar si es vol ordenar, serà Y o N
-        while (eleccio != 0) {
+        GestorDades gestor = new GestorDades();
+
+        int eleccio;       //Variable per guardar l'opció del menú secundari elegida per l'usuari.
+        int eleccio2;      //Variable per guardar l'opció del menú secundari elegida per l'usuari.
+        boolean exit1 = false;
+
+        while (!exit1) {
             Interficie.mostrarMenu(Interficie.opcmenu);
             eleccio = sc.nextInt();
             switch (eleccio) {
-                case 1:         //Entrem al menú secundari
-                    haconsultat++;
-                    while (eleccio2 != 0) {
-                        Interficie.mostrarMenu(Interficie.opcmenu2); //Mostrem el menú secundari i demanem l'opció.
+                case 1://CARREGA LES DADES ALS ARRAYLISTS
+                    gestor.carregarDades();
+                    break;
+                case 2:
+                    boolean exit2 = false;
+                    while (!exit2) {
+                        Interficie.mostrarMenu(Interficie.opcmenu2); //MOSTRAR SUBMENU PREGUNTANT SI L'USUARI VOL VEURE TOTS, ST O PROF
                         eleccio2 = sc.nextInt();
                         switch (eleccio2) {
-                            case 1://Mostrem les dades i afegim un element si l'usuari ho desitja.
-                                Interficie.mostrarDades(GestorDades.personatges);
-                                Interficie.mostrarMissatge("Vols afegir un element? Y/N:");
-                                sc.nextLine();
-                                afegirelement = sc.nextLine();
-                                id_consulta = 1;
-                                if (afegirelement.equals("Y") || afegirelement.equals("y")) {
-                                    Interficie.mostrarMissatge("Escriu l'element que vulguis afegir:");
-                                    nomelement = sc.nextLine();
-                                    GestorDades.afegirElement(dades, id_consulta, nomelement);
+
+                            case 1: //MOSTRAR TOTS ELS PERSONATGES (JUNTAR STU, AMB PROF)
+                                if (gestor.comprovarArraybuida(gestor.getDades().getEstudiants()) && gestor.comprovarArraybuida(gestor.getDades().getEstudiants())) {
+                                    System.out.println("Insereix dades a estudiants i professors!");
                                 } else {
+                                    Interficie.mostrarDades(gestor.getDades().getEstudiants());
+                                    Interficie.mostrarDades(gestor.getDades().getProfessors());
                                 }
                                 break;
-                            case 2:
-                                Interficie.mostrarDades(GestorDades.estudiants);
-                                Interficie.mostrarMissatge("Vols afegir un element? Y/N:");
-                                sc.nextLine();
-                                afegirelement = sc.nextLine();
-                                id_consulta = 2;
-                                if (afegirelement.equals("Y") || afegirelement.equals("y")) {
-                                    Interficie.mostrarMissatge("Escriu l'element que vulguis afegir:");
-                                    nomelement = sc.nextLine();
-                                    GestorDades.afegirElement(dades, id_consulta, nomelement);
+                            case 2: //MOSTAR ESTUDIANTS
+                                if (gestor.comprovarArraybuida(gestor.getDades().getEstudiants())) {
+                                    System.out.println("Insereix dades a estudiants!");
                                 } else {
+                                    Interficie.mostrarDades(gestor.getDades().getEstudiants());
                                 }
                                 break;
-                            case 3:
-                                Interficie.mostrarDades(GestorDades.professors);
-                                Interficie.mostrarMissatge("Vols afegir un element? Y/N:");
-                                sc.nextLine();
-                                afegirelement = sc.nextLine();
-                                id_consulta = 3;
-                                if (afegirelement.equals("Y") || afegirelement.equals("y")) {
-                                    Interficie.mostrarMissatge("Escriu l'element que vulguis afegir:");
-                                    nomelement = sc.nextLine();
-                                    GestorDades.afegirElement(dades, id_consulta, nomelement);
+                            case 3: //MOSTRAR PROFESSORS
+                                if (gestor.comprovarArraybuida(gestor.getDades().getProfessors())) {
+                                    System.out.println("Insereix dades a professors!");
                                 } else {
+                                    Interficie.mostrarDades(gestor.getDades().getProfessors());
                                 }
                                 break;
                             case 0:
+                                exit2 = true;
                                 break;
                             default:
                                 Interficie.mostrarMissatge("Inserta un número que correspongui a alguna opció:");
@@ -89,46 +69,34 @@ public class M3_UF3_ActAvaluable {
                         }
 
                     }
-                    eleccio2 = -1; //Hem d'asginar-li -1 a l'opció del submenú perquè al sortir d'ell es queda en 0 llavors si tornem a entrar dona problemes
-                case 2: //Demanem un nom per al fitxer, preguntem si es vol ordenar les dades i el creem i escribim o sobreescribim si ja està creat.
-                    if (eleccio == 2) {
-                        if (haconsultat != 0) {
-                            Interficie.mostrarMissatge("Inserta nom del fitxer:");
-                            String nomfitxer;
-                            sc.nextLine();
-                            nomfitxer = sc.nextLine();
-                            Interficie.mostrarMissatge("Vols ordenar els elements? Y/N");
-                            volsordenar = sc.nextLine();
-                            if (volsordenar.equals("Y") || volsordenar.equals("y")) {
-                                GestorDades.ordenarElements(dades);
-                            } else {
-                            }
-                            switch (id_consulta) {
-                                case 1:
-                                    GestorDades.desarDades(GestorDades.personatges, nomfitxer);
-                                    break;
-                                case 2:
-                                    GestorDades.desarDades(GestorDades.estudiants, nomfitxer);
-                                    break;
-                                case 3:
-                                    GestorDades.desarDades(GestorDades.professors, nomfitxer);
-                                    break;
-                            }
-                        } else {
-                            Interficie.mostrarMissatge("Has de fer una consulta per poder guardar!");
-                        }
-
-                    }
                     break;
                 case 3:
-                    Interficie.mostrarUsuaris();
-                case 0:
+                    System.out.println("Que vols afegir?");
+                    System.out.println("1.Alumnes");
+                    System.out.println("2.Professors");
+                    switch (sc.nextInt()) {
+                        case 1: //ALUMNES
+                            Interficie.preguntarAtributs("est", gestor);
+                            break;
+                        case 2: //PROFESSORS
+                            Interficie.preguntarAtributs("pro", gestor);
+                            break;
+                        case 0:
+                            eleccio2 = 0;
+                            break;
+                    }
                     break;
-                default:
-                    Interficie.mostrarMissatge("Inserta un número que correspongui a alguna opció:");
+                case 4: //Esborrar personatges (en el cas de que les arrays estiguin buides no permet borrar)
+                    if (gestor.comprovarArraybuida(gestor.getDades().getEstudiants()) && gestor.comprovarArraybuida(gestor.getDades().getProfessors())) {
+                        System.out.println("Insereix dades a estudiants i professors!");
+                    } else {
+                        System.out.println("Quin personatge vols esborrar?");
+                        gestor.buscareliminarPersonatge(sc.nextLine().toLowerCase());
+                    }
+                    break;
+                case 0:
+                    exit1 = true;
             }
-
         }
-        Interficie.mostrarMissatge("Bye");
     }
 }
